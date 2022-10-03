a7c5cd6561751cda614cca00a60b2b916cca31ab
ritesh2912 <94890713+ritesh2912@users.noreply.github.com>
2022-10-03 11:45:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+
 const app = express();
-const PORT = process.env.PORT || 8081;
 
-app.get('/', async (req, res) => {
-  const username = req.query.username || 'myogeshchavan97';
-  try {
-    const result = await axios.get(
-      `https://api.github.com/users/${username}/repos`
-    );
-    const repos = result.data
-      .map((repo) => ({
-        name: repo.name,
-        url: repo.html_url,
-        description: repo.description,
-        stars: repo.stargazers_count
-      }))
-      .sort((a, b) => b.stars - a.stars);
-
-    res.send(repos);
-  } catch (error) {
-    res.status(400).send('Error while getting list of repositories');
-  }
+app.get("/", (req, res) => {
+  res.send("This is my docker app");
 });
 
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
+app.listen(8080, () => {
+  console.log("listening");
 });
